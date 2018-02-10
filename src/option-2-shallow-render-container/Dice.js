43f1139456a2584e0fa9796198a0e452e5658612
Corina Udrescu <corinutz@gmail.com>
2018-02-10 18:06:57 +0100

    Update to option 2 - no longer export inner component since its not needed

--- src/option-2-shallow-render-container/Dice.js
+++ src/option-2-shallow-render-container/Dice.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { rollDice } from '../actions';
 import { connect } from 'react-redux';
 
-export const Dice = ({ lastRolledNumber, onRollDice }) => (
+const Dice = ({ lastRolledNumber, onRollDice }) => (
     <div>        
         <p>The last rolled number was {lastRolledNumber}.</p>
         <button onClick={onRollDice}>Roll dice</button>
