44c1ad1696d9d0c99963b27b2bc9c576a23542f7
Corina Udrescu <corinutz@gmail.com>
2018-05-21 13:24:01 +0200

    Added more code comments to the samples

--- src/option-1-export-mapDispatchToProps-and-mapStateToProps/Dice.test.js
+++ src/option-1-export-mapDispatchToProps-and-mapStateToProps/Dice.test.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// We made the mapStateToProps and mapDispatchToProps methods public, 
+// so they can now be imported in the test
 import { mapStateToProps, mapDispatchToProps } from './Dice';
 
 describe('Dice', () => {
@@ -6,11 +8,17 @@ describe('Dice', () => {
         const initialState = {
             lastRolledNumber: 1
         };
+
+        // Just call the method directly passing in sample data
+        // to make sure it does what it's supposed to
         expect(mapStateToProps(initialState).lastRolledNumber).toEqual(1);
     });
 
     it('should roll the dice again when button is clicked', () => {
         const dispatch = jest.fn();
+
+        // For the `mapDispatchToProps`, call it directly but pass in
+        // a mock function and check the arguments passed in are as expected
         mapDispatchToProps(dispatch).onRollDice();
         expect(dispatch.mock.calls[0][0]).toEqual({ type: 'ROLL_DICE'});
     });
--- src/option-2-shallow-render-container/Dice.test.js
+++ src/option-2-shallow-render-container/Dice.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import '../test-config';
+import '../test-config'; // Setup Enzyme & Adapter
 
 import DiceContainer from './Dice';
+
+// Create the mock store
 import configureMockStore from 'redux-mock-store';
 const mockStore = configureMockStore();
 
@@ -14,16 +16,19 @@ describe('Dice', () => {
             lastRolledNumber: 1
         };
         store = mockStore(initialState);
+        // Shallow render the container passing in the mock store
         wrapper = shallow(
             <DiceContainer store={store} />
         );
     });
 
     it('should show previously rolled value', () => {
+        // test that the state values were correctly passed as props
         expect(wrapper.props().lastRolledNumber).toBe(1);
     });
 
     it('should roll the dice again when button is clicked', () => {
+        // test that the component events dispatch the expected actions 
         wrapper.simulate('rollDice');
 
         const actions = store.getActions();
